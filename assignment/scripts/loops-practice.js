6cc0336a2de6de553579aa36c62818a617050979
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-09 15:13:44 -0600

    Added alternate answers which only include the specific numbers in question, unlike the initial example.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -16,7 +16,14 @@ console.log('count from 0 to 5');
 /*
 We would need to change i<4 to i<6
 */
-for (let i = 0; i < 6; i++) {
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 1.a. that only includes the numbers in question');
+for (let i=0; i<=5; i++) {
   console.log(i);
 }
 
@@ -29,7 +36,14 @@ We would need to change i=0 to i=3
 and
 We would need to change i<4 to i<6
 */
-for (let i = 3; i < 6; i++) {
+for (let i=3; i<6; i++) {
+  console.log(i);
+}
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 1.b. that only includes the numbers in question');
+for (let i=3; i<=5; i++) {
   console.log(i);
 }
 
@@ -46,10 +60,27 @@ We would need to change i++ to i+=2
 for (let i=2; i<11; i+=2) {  
   console.log(i);
 }
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 1.c. that only includes the numbers in question (and plays with remainder to find if even)');
+for (let i=2; i<=10; i+=2) {
+  if (i%2 === 0) { 
+    console.log(i);
+  } //This checks if i is divided by 2 and the remainder is 0, then if would log the initial value of i.
+    //We could use this to instead increment 1 at a time or 3 at a time and still consistely only log the even numbers.
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (let i=5; i>-1; i--) {  
+for (let i = 5; i >- 1; i--) {  
+  console.log(i);
+}
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 1.d. that only includes the numbers in question');
+for (let i = 5; i >= 0; i--) {  
   console.log(i);
 }
 
@@ -63,6 +94,7 @@ console.log('Some stars:');
 for (star of stars) {
   console.log(star);
 }
+
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
@@ -84,6 +116,15 @@ while (n<6) {
   console.log(n);
   n++;
 }
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 3.b. that only includes the numbers in question');
+let N=0
+while (N<=5) {
+  console.log(N);
+  N++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
@@ -91,4 +132,13 @@ let x=10
 while (x>4) {
   console.log(x);
   x--
+}
+/*
+Another version with only utilizing the numbers in question
+*/
+console.log('Here is another version of 3.c. that only includes the numbers in question');
+let X=10
+while (X>=5) {
+  console.log(X);
+  X--
 }
